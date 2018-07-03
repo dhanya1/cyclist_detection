798af0a89dcce488dd8346a7f77792c6dff8c8e7
dsj1 <dhanyasj01@gmail.com>
2018-07-03 16:01:39 +0100

    Upagraded record creation

--- custom_scripts/image_to_tfrecord.py
+++ custom_scripts/image_to_tfrecord.py
@@ -1,128 +1,141 @@
 import io
 import hashlib
-import xml.etree.ElementTree as ET
 import tensorflow as tf
 import random
 import os
-import re
 import json
-
 from PIL import Image
 from object_detection.utils import dataset_util
 
 '''
-this script automatically divides dataset into training and evaluation (10% for evaluation)
-this scripts also shuffles the dataset before converting it into tfrecords
-if u have different structure of dataset (rather than pascal VOC ) u need to change
-the paths and names input directories(images and annotation) and output tfrecords names.
-(note: this script can be enhanced to use flags instead of changing parameters on code).
+This scripts shuffles the dataset before converting it into tfrecords.
 
 default expected directories tree:
 dataset- 
-   -JPEGImages
-   -Annotations
-    dataset_to_tfrecord.py   
+   -PNGImages
+   -JSON Annotations
+  image_to_tfrecord.py   
 
 
 to run this script:
-$ python dataset_to_tfrecord.py 
+$ python image_to_tfrecord.py -a train (for training)
+$ python image_to_tfrecord.py -a test (for training)
 
 '''
 
+class CreateTFRecord():
+    all_records = {}
+    def __init__(self):
+        self.width = int(2048)
+        self.height = int(1024)
+        self.xmin = []
+        self.ymin = []
+        self.xmax = []
+        self.ymax = []
+        self.classes = []
+        self.classes_text = []
+        self.image_format = b'png'
 
-def create_example(xml_file):
-    # process the xml file
-    tree = ET.parse(xml_file)
-    root = tree.getroot()
-    image_name = root.find('imagename').text
-    image_format = b'png'
-    file_name = image_name.encode('utf8')
-    width = int(2048)
-    height = int(1024)
-    xmin = []
-    ymin = []
-    xmax = []
-    ymax = []
-    classes = []
-    classes_text = []
-    track_id = []
-    unique_id = []
-    for member in root.findall('children'):
-        xmax.append(float(member[0][0].text)/width)
-        #track_id.append(member[0][2].text.encode('utf8'))
-        xmin.append(float(member[0][3].text)/ width)
-        ymin.append(float(member[0][4].text)/height)
-        ymax.append(float(member[0][5].text)/height)
-        #unique_id.append(member[0][6].text.encode('utf8'))
-        classes_text.append('cyclist'.encode('utf8'))
-        classes.append(1)
-        # i wrote 1 because i have only one class(cyclist)
-        #  read related image
-    images_dir = '/users/mscdsa2018/dsj1/PycharmProjects/action/data/tsinghuaDaimlerScripts/tar_folder/leftImg8bit/train/tsinghuaDaimlerDataset'
-    full_path = os.path.join(images_dir, '{}'.format(image_name))
-    print(full_path)# provide the path of images directory
-    with tf.gfile.GFile(full_path, 'rb') as fid:
-        encoded_png = fid.read()
-    encoded_png_io = io.BytesIO(encoded_png)
-    image = Image.open(encoded_png_io)
-    if image.format != 'PNG':
-        raise ValueError('Image format not PNG')
-    key = hashlib.sha256(encoded_png).hexdigest()
-
-    # create TFRecord Example
-    example = tf.train.Example(features=tf.train.Features(feature={
-        'image/height': dataset_util.int64_feature(height),
-        'image/width': dataset_util.int64_feature(width),
-        'image/filename': dataset_util.bytes_feature(file_name),
-        'image/source_id': dataset_util.bytes_feature(file_name),
-        'image/key/sha256': dataset_util.bytes_feature(key.encode('utf8')),
-        'image/encoded': dataset_util.bytes_feature(encoded_png),
-        'image/format': dataset_util.bytes_feature(image_format),
-        'image/object/bbox/xmin': dataset_util.float_list_feature(xmin),
-        'image/object/bbox/xmax': dataset_util.float_list_feature(xmax),
-        'image/object/bbox/ymin': dataset_util.float_list_feature(ymin),
-        'image/object/bbox/ymax': dataset_util.float_list_feature(ymax),
-        'image/object/class/text': dataset_util.bytes_list_feature(classes_text),
-        'image/object/class/label': dataset_util.int64_list_feature(classes),
-        #'image/children/item/trackid': dataset_util.bytes_list_feature(track_id),
-        #'image/children/item/uniqueid': dataset_util.bytes_list_feature(unique_id),
-    }))
-    return example
+    def __reading_json(self, json_file):
+        with open(json_file, 'r') as file:
+            json_content = file.read()
+        self.json_Dict = json.loads(json_content)
+        image_name = self.json_Dict['imagename']
+        CreateTFRecord.all_records[image_name] = self
 
 
-def main(_):
+    def create_records(self, images_dir, json_dir):
+        '''
+        :param image_dir: Give full path of the directory that has test images
+        :param json_dir:  Give full path of the directory that has test annotations
+        :return:
+        '''
+        self.__reading_json(json_dir)
+        self.image_name = self.json_Dict['imagename']
+        self.full_path = os.path.join(images_dir, '{}'.format(self.image_name))
+        list_of_objects = self.json_Dict['children']
+        for objects in list_of_objects:
+            self.xmax.append(float(objects['maxcol'])/self.width)
+            self.xmin.append(float(objects['mincol'])/ self.width)
+            self.ymin.append(float(objects['minrow'])/self.height)
+            self.ymax.append(float(objects['maxrow'])/self.height)
+            self.classes_text.append(objects['identity'].encode('utf8'))
+            self.classes.append(1)
+        example = self.__add_features()
+        return example
+
+    def __add_features(self):
+        # create TFRecord Example
+        with tf.gfile.GFile(self.full_path, 'rb') as fid:
+            encoded_png = fid.read()
+        encoded_png_io = io.BytesIO(encoded_png)
+        image = Image.open(encoded_png_io)
+        if image.format != 'PNG':
+            raise ValueError('Image format not PNG')
+        key = hashlib.sha256(encoded_png).hexdigest()
+        example = tf.train.Example(features=tf.train.Features(feature={
+            'image/height': dataset_util.int64_feature(self.height),
+            'image/width': dataset_util.int64_feature(self.width),
+            'image/filename': dataset_util.bytes_feature(self.image_name.encode('utf8')),
+            'image/source_id': dataset_util.bytes_feature(self.image_name.encode('utf8')),
+            'image/key/sha256': dataset_util.bytes_feature(key.encode('utf8')),
+            'image/encoded': dataset_util.bytes_feature(encoded_png),
+            'image/format': dataset_util.bytes_feature(self.image_format),
+            'image/object/bbox/xmin': dataset_util.float_list_feature(self.xmin),
+            'image/object/bbox/xmax': dataset_util.float_list_feature(self.xmax),
+            'image/object/bbox/ymin': dataset_util.float_list_feature(self.ymin),
+            'image/object/bbox/ymax': dataset_util.float_list_feature(self.ymax),
+            'image/object/class/text': dataset_util.bytes_list_feature(self.classes_text),
+            'image/object/class/label': dataset_util.int64_list_feature(self.classes),
+            # 'image/children/item/trackid': dataset_util.bytes_list_feature(track_id),
+            # 'image/children/item/uniqueid': dataset_util.bytes_list_feature(unique_id),
+        }))
+        return example
+
+def writing_train():
     writer_train = tf.python_io.TFRecordWriter('train.record')
-    writer_test = tf.python_io.TFRecordWriter('test.record')
-    # provide the path to annotation xml files directory
-    xml_files = '/users/mscdsa2018/dsj1/PycharmProjects/action/data/tsinghuaDaimlerScripts/tar_folder/labelData/train/tsinghuaDaimlerDataset/xml'+'/*.xml'
-    filename_list = tf.train.match_filenames_once(xml_files)
+    json_files = '/home/tar_folder/labelData/train/tsinghuaDaimlerDataset/*.json'
+    images_dir = '/home/tar_folder/labelData/train/leftImg8bit/train/tsinghuaDaimlerDataset'
+    filename_list = tf.train.match_filenames_once(json_files)
     init = (tf.global_variables_initializer(), tf.local_variables_initializer())
     sess = tf.Session()
     sess.run(init)
     list = sess.run(filename_list)
     random.shuffle(list)  # shuffle files list
-    i = 1
-    tst = 0  # to count number of images for evaluation
-    trn = 0  # to count number of images for training
-    for xml_file in list:
-        example = create_example(xml_file)
-        if (i % 10) == 0:  # each 10th file (xml and image) write it for evaluation
-            writer_test.write(example.SerializeToString())
-            tst = tst + 1
-        else:  # the for training
-            writer_train.write(example.SerializeToString())
-            trn = trn + 1
-        i = i + 1
-        print(xml_file)
-    writer_test.close()
+    for json_file in list:
+        obj = CreateTFRecord(images_dir, json_file)
+        example = obj.create_records(json_file)
+        writer_train = tf.python_io.TFRecordWriter('train.record')
+        writer_train.write(example.SerializeToString())
     writer_train.close()
-    print('Successfully converted dataset to TFRecord.')
-    print('training dataset: # ')
-    print(trn)
-    print('test dataset: # ')
-    print(tst)
 
+
+def writing_test():
+    # provide the path to annotation xml files directory
+    json_files = '/home/tar_folder/labelData/test/tsinghuaDaimlerDataset/*.json'
+    images_dir = '/home/tar_folder/leftImg8bit/test/tsinghuaDaimlerDataset'
+    filename_list = tf.train.match_filenames_once(json_files)
+    init = (tf.global_variables_initializer(), tf.local_variables_initializer())
+    sess = tf.Session()
+    sess.run(init)
+    list = sess.run(filename_list)
+    random.shuffle(list)  # shuffle files list
+    for json_file in list:
+        obj = CreateTFRecord()
+        example = obj.create_records(images_dir, json_file)
+        writer_test = tf.python_io.TFRecordWriter('test.record')
+        writer_test.write(example.SerializeToString())
+    writer_test.close()
+
+def main(arguments):
+    action = input('Enter test or train: ').strip()
+    if action.lower() == 'train':
+        writing_train()
+    elif action.lower() == 'test':
+        writing_test()
+    else:
+        print('Invalid input. Enter test or train')
 
 if __name__ == '__main__':
     tf.app.run()
-
+    print('Success')
